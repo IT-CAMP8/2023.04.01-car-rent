df69949fd635d8f2fb27a63be72a8b88312b49a8
Mateusz <bmatteo@interia.pl>
2023-04-01 20:46:27 +0200

    init

---
+++ src/pl/camp/it/car/rent/db/CarRepository.java
@@ -0,0 +1,33 @@
+package pl.camp.it.car.rent.db;
+
+import pl.camp.it.car.rent.model.Car;
+
+public class CarRepository {
+    private Car[] cars = new Car[5];
+
+    CarRepository() {
+        this.cars[0] = new Car("BMW", "3", 2015,
+                300.0, "KR11");
+        this.cars[1] = new Car("Audi", "A5", 2016,
+                350.0, "KR22");
+        this.cars[2] = new Car("Kia", "Ceed", 2017,
+                320.0, "KR33");
+        this.cars[3] = new Car("Toyota", "Corolla",
+                2018, 370.0, "KR44");
+        this.cars[4] = new Car("Volvo", "V50", 2013,
+                200.0, "KR55");
+    }
+
+    public void rentCar(String plate) {
+        for(Car car : this.cars) {
+            if(!car.isRent() && car.getPlate().equals(plate)) {
+                car.setRent(true);
+                return;
+            }
+        }
+    }
+
+    public Car[] getCars() {
+        return cars;
+    }
+}
